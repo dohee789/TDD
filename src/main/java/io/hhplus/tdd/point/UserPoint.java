e2ce40c7acbb23bf9f9d2ee3e9b052c2e518a7db
dohee789 <dohee@doheeui-MacBookPro.local>
2025-03-26 21:39:14 +0900

    feat: UserPoint 충전/사용 도메인 로직, 테스트 코드

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -9,4 +9,28 @@ public record UserPoint(
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
     }
+
+    public UserPoint charge(long point) {
+        if (point <= 0) {
+            throw new IllegalArgumentException("최소 충전 금액은 1포인트 입니다");
+        }
+        long maxPoint = 1000L;
+        long currentPoint = this.point() + point;
+        if (currentPoint > maxPoint) {
+            throw new IllegalArgumentException("최대 잔고 제한을 초과했습니다. 현재 포인트: " + currentPoint);
+        }
+        return new UserPoint(this.id(), currentPoint, System.currentTimeMillis());
+    }
+
+    public UserPoint use(long point) {
+        if (point <= 0) {
+            throw new IllegalArgumentException("최소 사용 금액은 1포인트 입니다");
+        }
+        long minPoint = 0L;
+        long currentPoint = this.point() - point;
+        if (currentPoint < minPoint) {
+            throw new IllegalArgumentException("포인트 잔액이 부족합니다. 현재 포인트: " + currentPoint);
+        }
+        return new UserPoint(this.id(), minPoint, System.currentTimeMillis());
+    }
 }
---
+++ src/test/java/io/hhplus/tdd/point/UserPointTest.java
@@ -0,0 +1,70 @@
+package io.hhplus.tdd.point;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+class UserPointTest {
+    static final long USER_ID = 1L;
+
+    @DisplayName("충전할 포인트가 0미만 경우 IllegalArgumentException 이 발생합니다.")
+    @Test
+    void chargePointException() {
+        // given
+        UserPoint userPoint = UserPoint.empty(USER_ID);
+        long chargePoint = -1L;
+        // when
+        Throwable exception = catchThrowable(() -> userPoint.charge(chargePoint));
+        // then
+        assertThat(exception)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("최소 충전 금액은 1포인트 입니다");
+    }
+
+    @DisplayName("충전 최대 잔고를 초과할 경우 IllegalArgumentException 이 발생합니다.")
+    @Test
+    void maxChargedPointException() {
+        // given
+        long currentPoint = 1000L;
+        long chargePoint = 1L;
+        UserPoint userPoint = new UserPoint(USER_ID, currentPoint, System.currentTimeMillis()); // 최대 충전 금액으로 세팅
+        // when
+        Throwable exception = catchThrowable(() -> userPoint.charge(chargePoint));
+        // then
+        assertThat(exception)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("최대 잔고 제한을 초과했습니다. 현재 포인트: " + (userPoint.point() + chargePoint));
+    }
+
+    @DisplayName("사용할 포인트가 0미만 경우 IllegalArgumentException 이 발생합니다.")
+    @Test
+    void usePointException() {
+        // given
+        UserPoint userPoint = UserPoint.empty(USER_ID);
+        long chargePoint = -1L;
+        // when
+        Throwable exception = catchThrowable(() -> userPoint.use(chargePoint));
+        // then
+        assertThat(exception)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("최소 사용 금액은 1포인트 입니다");
+    }
+
+    @DisplayName("포인트 잔액이 부족할 경우 IllegalArgumentException 이 발생합니다.")
+    @Test
+    void minChargedPointException() {
+        // given
+        long currentPoint = 0L;
+        long usePoint = 1L;
+        UserPoint userPoint = new UserPoint(USER_ID, currentPoint, System.currentTimeMillis()); // 최소 잔액으로 세팅
+        // when
+        Throwable exception = catchThrowable(() -> userPoint.use(usePoint));
+        // then
+        assertThat(exception)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("포인트 잔액이 부족합니다. 현재 포인트: " + (userPoint.point() - usePoint));
+    }
+
+}
