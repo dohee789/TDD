0aefe645a8993951b3b1db267b66c8ace9c4d9f2
dohee789 <dohee@doheeui-MacBookPro.local>
2025-03-27 20:05:58 +0900

    chore: 상수로 변경

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -5,32 +5,32 @@ public record UserPoint(
         long point,
         long updateMillis
 ) {
+    private static final long MAX_POINT = 1000L;
+    private static final long MIN_POINT = 0L;
 
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
     }
 
-    public UserPoint charge(long point) {
-        if (point < 1) {
+    public UserPoint charge(long amount) {
+        if (amount < 1) {
             throw new IllegalArgumentException("최소 충전 금액은 1포인트 입니다");
         }
-        long maxPoint = 1000L;
-        long currentPoint = this.point() + point;
-        if (currentPoint > maxPoint) {
+        long currentPoint = this.point() + amount;
+        if (currentPoint > MAX_POINT) {
             throw new IllegalArgumentException("최대 잔고 제한을 초과했습니다. 현재 포인트: " + currentPoint);
         }
         return new UserPoint(this.id(), currentPoint, System.currentTimeMillis());
     }
 
-    public UserPoint use(long point) {
-        if (point < 1) {
+    public UserPoint use(long amount) {
+        if (amount < 1) {
             throw new IllegalArgumentException("최소 사용 금액은 1포인트 입니다");
         }
-        long minPoint = 0L;
-        long currentPoint = this.point() - point;
-        if (currentPoint < minPoint) {
+        long currentPoint = this.point() - amount;
+        if (currentPoint < MIN_POINT) {
             throw new IllegalArgumentException("포인트 잔액이 부족합니다. 현재 포인트: " + currentPoint);
         }
-        return new UserPoint(this.id(), minPoint, System.currentTimeMillis());
+        return new UserPoint(this.id(), currentPoint, System.currentTimeMillis());
     }
 }
